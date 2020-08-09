0e21d0a04a6a2527e406633fb14f38d3103010e9
abineko2 <yuki1980426@gmail.com>
2020-08-09 15:19:55 +0900

    header add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>{
+  return(
+    <div>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a className="navbar-brand text-white font-weight-bold" href="#">商品確認</a>
+        </nav>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
 export default App;
